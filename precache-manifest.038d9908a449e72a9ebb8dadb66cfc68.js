1a15c7a41ab44d3127485a044fb3beeead90d26c
Sihyun Kim <legitcode267@gmail.com>
2020-01-05 19:44:28 -0700

    Updates

---
+++ precache-manifest.038d9908a449e72a9ebb8dadb66cfc68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1d21d3467c11111ef91d25a91ad656",
+    "url": "/react-random-shiba/index.html"
+  },
+  {
+    "revision": "5e3b8889e5f8333ecc1a",
+    "url": "/react-random-shiba/static/css/main.cfa53a37.chunk.css"
+  },
+  {
+    "revision": "2c1423a48ce0cdbc283a",
+    "url": "/react-random-shiba/static/js/2.238c08ff.chunk.js"
+  },
+  {
+    "revision": "e5a443e119b83eb07842502589f56485",
+    "url": "/react-random-shiba/static/js/2.238c08ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e3b8889e5f8333ecc1a",
+    "url": "/react-random-shiba/static/js/main.e82eafbb.chunk.js"
+  },
+  {
+    "revision": "2892504bdffef7612c20",
+    "url": "/react-random-shiba/static/js/runtime-main.ea555759.js"
+  }
+]);
