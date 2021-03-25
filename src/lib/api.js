175a0bd9d788b444513d89ca226599c21acf3668
Sihyun Kim <k4sud0n@gmail.com>
2021-03-25 20:52:32 +0900

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export function getAPI() {
-    return axios.get('https://cors-anywhere.herokuapp.com/shibe.online/api/shibes?count=1&urls=true&httpsUrls=true');
+    return axios.get('https://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true');
 }
